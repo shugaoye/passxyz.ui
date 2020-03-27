180834bda85e7f1544c158c611fc30a11b7dc915
Roger Ye <shugaoye@yahoo.com>
2020-03-27 17:53:47 +0800

    Added MarkdownContentView.

---
+++ PassXYZ.UI.Abstractions/MarkdownContentView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PassXYZ.UI.Abstractions
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MarkdownContentView : ContentView
+    {
+        public MarkdownContentView()
+        {
+            InitializeComponent();
+
+            markdownView.Markdown = "Please take your notes.";
+            markdownView.VerticalOptions = LayoutOptions.FillAndExpand;
+        }
+
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(
+            propertyName: "Text",
+            returnType: typeof(string),
+            declaringType: typeof(MarkdownContentView),
+            defaultValue: default(string));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { markdownView.Markdown = value; SetValue(TextProperty, value); }
+        }
+
+    }
+}
